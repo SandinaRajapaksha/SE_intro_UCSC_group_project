d067d30aca21046b31b0e28980aa90f84ea16576
Sandina Rajapaksha <tech.sandina@gmail>
2026-05-05 23:43:42 +0530

    5

--- main.py
+++ main.py
@@ -1,51 +1,26 @@
-#user class
-class UserClass:
-    name : "default" # type: ignore
-    passwordUsr : 1234
-    balance : 0
-
-listOfusers = []
-
-
-def alreadyRegisteredMenu():
-    option = input("Are you a registes user : (y)yes , (n)o , (e)exit")
-    match option:
-        case "y" : passcheck()
-        case "n" : registerUser()
-        case _ : alreadyRegisteredMenu()
-
-def passcheck():
-    userName = input("enter user name : ")
-    password = input("enter password : ")
-    for i in listOfusers:
-        if userName == i:
-            break
-        else :
-            passcheck()
+db = {}
+def addUserToDB(name,passw):
+    db[name] = {"PSWD":passw,"BLNC": 0}
+    print(db)
     
-    if password == userName.passwordUsr:
-        optionselect()
-    else:
-        print("incorrect password, enter any key to contiue")
-        input()
-        alreadyRegisteredMenu()
-
-def registerUser():
-    newUsername = input("enter new username")
-    newPassword = input("enter new password")
-    userobject = UserClass(newUsername,newPassword)
-    license.append(userobject)
-    print("successfully registered, enter any key to continue")
-    input()
-    alreadyRegisteredMenu()
-
-def optionselect():
-    print("select option : ")
-    print("(d)eposit money\n(w)ithdraw money\n(c)heck balance\n(e)xit")
-    option = input()
-    match option:
-        case "d" : deposit()
-        case "w" : withdraw()
-        case "c" : checkBalance()
-        case _ : alreadyRegisteredMenu() 
     
+def main():
+    registerState = input("Are you a registered user (y)es , (n)o : ")
+    if registerState == "n" :
+        Newusername = input("Enter new user name : ")
+        Newpass = input("Enter new password : ")
+        addUserToDB(Newusername,Newpass)
+        main()    
+
+    elif registerState == "y" :
+        pscheckFlag = False
+        while pscheckFlag == False:
+            userName = input("Enter username : ")        
+            password = input("Enter password : ")
+            pscheckFlag = passchecker(userName,password)
+            if pscheckFlag == True:
+                break
+        optionMenu()
+
+main()
+
